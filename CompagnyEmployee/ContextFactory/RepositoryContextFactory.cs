237b482855a295e7c74b4957c2da89577fa2fe37
younesbelarbi <ybelarbi@outlook.com>
2023-06-18 19:54:34 +0200

    crétion du context pour acces base de donne 3.3 Context Class and the Database Connection

---
+++ CompagnyEmployee/ContextFactory/RepositoryContextFactory.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Repository;
+
+namespace CompagnyEmployee.ContextFactory
+{
+    /// <summary>
+    /// Étant donné que notre classe RepositoryContext est dans un projet Repository et non dans le projet principal, cette classe aidera notre application à créer une instance DbContext dérivée au moment de la conception, 
+    /// ce qui nous aidera dans nos migrations.
+    /// Nous utilisons l'interface IDesignTimeDbContextFactory<out TContext> qui permet aux services de conception de découvrir les implémentations de cette interface. Bien sûr, le paramètre TContext est notre classe RepositoryContext.
+    /// </summary>
+    public class RepositoryContextFactory : IDesignTimeDbContextFactory<RepositoryContext>
+    {
+        public RepositoryContext CreateDbContext(string[] args)
+        {
+            //on specifie le app setting qui doit être utilisé
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            //nous pouvons utiliser la méthode GetConnectionString pour accéder à la chaîne de connexion à partir du fichier appsettings.json.
+            var builder = new DbContextOptionsBuilder<RepositoryContext>()
+                .UseSqlServer(configuration.GetConnectionString("sqlConnection"));
+
+            //nous renvoyons une nouvelle instance de notre classe RepositoryContext avec les options fournies.
+            return new RepositoryContext(builder.Options);
+        }
+    }
+}
---
+++ Repository/RepositoryContext.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Repository
+{
+    /// <summary>
+    /// cette classe de contexte sera un composant middleware pour la communication avec la base de données.
+    /// elle doit hériter de la classe DbContext d'Entity Framework Core et se compose de propriétés DbSet, qu'EF Core va utiliser pour la communication avec la base de données
+    /// </summary>
+    public class RepositoryContext : DbContext
+
+    {
+        public RepositoryContext(DbContextOptions options) : base (options) 
+        {            
+        }
+
+        public DbSet<Company>? Compagnies { get; set; }
+        public DbSet<Employee> MyProperty { get; set; }
+    }
+}
